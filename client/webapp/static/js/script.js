b2efd847c4c6038d2aa138c90d8ffa46f5a53592
unknown <masterharoldsaxon4@gmail.com>
2020-06-10 09:25:16 +0300

    fix :pressing enter reloaded page

---
+++ client/webapp/static/js/script.js
@@ -0,0 +1,41 @@
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: '',
+    movieTitle: "",
+    info: {},
+  },
+  methods: {
+    handleSubmit: function Search(params) {
+      params.preventDefault()
+      console.log("send: ", this.movieTitle)
+      axios
+        .get('/s?q=' + this.movieTitle)
+        .then(response => {
+          this.info = response.data;
+          console.log("data", this.info.count);
+          console.log("inf", this.info.filminfo.length);
+          mass = "" 
+          cd='<div>'
+          for (let i = 0; i < this.info.filminfo.length; i++) { // выведет 0, затем 1, затем 2
+            console.log("len:", i, ") ", this.info.filminfo.length)
+            mass = mass +genItem(this.info.filminfo[i])
+
+          }
+          this.message = "Result for: " + this.movieTitle +  '<div class="result">'+'<div class="resultItem"> ' + mass + '</div>'+ '</div>'
+        })
+        .catch(error => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+
+    },
+
+    handleChange(event) {
+      this.movieTitle = event.target.value
+      console.log(this.movieTitle)
+    }
+  },
+})
